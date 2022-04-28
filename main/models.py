f1c14f21a512b90e7f8e68a442d29a09adf8f4f5
stasofob <andreeff.stas2014@gmail.com>
2022-04-29 01:56:07 +0300

    добавил модели

--- main/models.py
+++ main/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import RegexValidator
 
 class MailList(models.Model):
     datetime_start = models.DateTimeField(verbose_name='Дата и время запуска рассылки')
     text = models.TextField(verbose_name='Текст сообщения для доставки клиенту')
     datetime_stop = models.DateTimeField(verbose_name='Дата и время окончания рассылки')
 
-class Client(model.Model):
+class Client(models.Model):
+    phoneNumberRegex = RegexValidator(regex = r'^7\d{10}$')
+    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=11, unique=True, null=False)
+    codeOperator = models.CharField(max_length=3, null=False)
+    tag = models.CharField(max_length=32)
+    timezone = models.CharField(max_length=32, choices=settings.TIMEZONES,
+                                default='UTC')
 
+class Message(models.Model):
+    datetime_sending = models.DateTimeField(verbose_name='Дата отправки')
+    status_sending = models.IntegerField(verbose_name='Статус отправки')
+    id_maillist = models.ForeignKey(MailList, on_delete=models.CASCADE)
+    id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
 
--- message_service/settings.py
+++ message_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import pytz
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
