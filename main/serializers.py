068aa55b02251420acce774bdaa1146c0fac46dd
stasofob <andreeff.stas2014@gmail.com>
2022-05-01 22:45:21 +0300

    Create serializers.py

---
+++ main/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Message, Client, MobileOperator, MailList, GroupClients
+
+
+class MailListSerializer(ModelSerializer):
+    class Meta:
+        model = MailList
+        fields = "__all__"
+
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class MobileOperatorSerializer(ModelSerializer):
+    class Meta:
+        model = MobileOperator
+        fields = "__all__"
+
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = "__all__"
+        
