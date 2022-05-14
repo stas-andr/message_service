b24e492eb8faea06f97afe860735091d3d498182
stasofob <andreeff.stas2014@gmail.com>
2022-05-14 22:23:56 +0300

    сигнал приходит, условие отправки проверяется

--- main/service.py
+++ main/service.py
@@ -1,19 +1,31 @@
+import pytz
 from datetime import datetime
 from django.db.models import Q
+from django.conf import settings
 from message_service.tasks import send_message
 
 from .models import MailList, Client, GroupClients
+
+timezone = pytz.timezone(settings.TIME_ZONE)
+
 
 def post_save_maillist(sender, instance:MailList, created,  **kwargs):
     print("post_save_maillist")
     if created:
         filter = instance.filter
-        mobile_operators = filter["mobile_operators"]
-        tags = filter["tags"]
+        mobile_operators = []
+        tags = []
+        try:
+            mobile_operators = filter["mobile_operators"]
+            tags = filter["tags"]
+        except KeyError:
+            # одно из полей точно заполнено
+            pass
         clients = Client.objects.filter(Q(tag__name__in=tags) | Q(mobile_operator__name__in=mobile_operators))
-        if datetime.now() <= instance.datetime_stop and datetime.now() >= instance.datetime_stop:
+        datetime_now = timezone.localize(datetime.now())
+        if datetime_now <= instance.datetime_stop and datetime_now >= instance.datetime_start:
             # отправить сообщения из рассылки
-            pass
-        elif datetime.now() < instance.datetime_start:
+            print("отправить сообщение из рассылки")
+        elif datetime_now < instance.datetime_start:
             # отложить отправление сообщения
-            pass
+            print("отложить отправление сообщения")
