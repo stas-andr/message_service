8d4a7f6950574a5514f50a1cc7305a57e78c48a4
stasofob <andreeff.stas2014@gmail.com>
2022-05-15 20:54:38 +0300

    создание сообщений
    Object of type Message is not JSON serializable

--- main/service.py
+++ main/service.py
@@ -4,28 +4,38 @@ from django.db.models import Q
 from django.conf import settings
 from message_service.tasks import send_message
 
-from .models import MailList, Client, GroupClients
+from .models import MailList, Client, GroupClients, Message
 
 timezone = pytz.timezone(settings.TIME_ZONE)
 
+
+def create_messages(mail_list, clients, sending_datetime):
+    """
+    create and send messages for clients in mail_list in sending_datetime
+    """
+    for client in clients:
+        message = Message(client=client, datetime_sending=datetime.now(), mail_list=mail_list)
+        send_message.apply_async((message, mail_list.interval_minutes_failed_message), eta=sending_datetime)
 
 def post_save_maillist(sender, instance:MailList, created,  **kwargs):
     print("post_save_maillist")
     if created:
         filter = instance.filter
-        mobile_operators = []
-        tags = []
         try:
             mobile_operators = filter["mobile_operators"]
+        except KeyError:
+            mobile_operators = []
+        try:
             tags = filter["tags"]
         except KeyError:
-            # одно из полей точно заполнено
-            pass
-        clients = Client.objects.filter(Q(tag__name__in=tags) | Q(mobile_operator__name__in=mobile_operators))
+            tags = []
+        clients = Client.objects.filter(Q(tag__tag__in=tags) | Q(mobile_operator__name__in=mobile_operators))
         datetime_now = timezone.localize(datetime.now())
         if datetime_now <= instance.datetime_stop and datetime_now >= instance.datetime_start:
             # отправить сообщения из рассылки
             print("отправить сообщение из рассылки")
+            create_messages(instance, clients, datetime_now)
         elif datetime_now < instance.datetime_start:
             # отложить отправление сообщения
             print("отложить отправление сообщения")
+            create_messages(instance, clients, instance.datetime_start)
--- message_service/tasks.py
+++ message_service/tasks.py
@@ -3,6 +3,7 @@ import json
 from .celery import app
 from django.conf import settings
 from celery.utils.log import get_task_logger
+from datetime import datetime
 
 logger = get_task_logger(__name__)
 
@@ -12,13 +13,14 @@ def send_message(self, message, minuts_retry_sending):
     message_phone = message.client.phone_number
     message_text = message.mail_list.text
     data_json = json.loads(f'{{"id": {message_id}, "phone": {message_phone}, "text": {message_text}}}')
+    message.datetime_sending = datetime.now() # для обновления значения в случае неотправки
     try:
         requests.post(url=settings.URL_SERVICE_SEND_MESSAGE+str(message.pk),
                       headers=settings.AUTH_HEADERS, json=data_json)
     except requests.exceptions.RequestException as exc:
         logger.info(f'Sending message with id: {message_id} failed, repeat sending in {minuts_retry_sending}')
         message.status_sending = 0 # статус: ошибка при отправке
-        self.retry(exc=exc)
+        self.retry(exc=exc, countdown=minuts_retry_sending*60)
     else:
         logger.info(f'Message with id: {message_id} has been sent')
         message.status_sending = 1 # статус: отправлено
